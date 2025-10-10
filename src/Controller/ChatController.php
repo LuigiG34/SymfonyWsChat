60c293fbead9044c9c50b7c8dd48efa5f1be9a01
Luigi Gandemer <luigigandemer6@gmail.com>
2025-10-10 19:49:16 +0200

    Controller / Template

---
+++ src/Controller/ChatController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ChatController extends AbstractController
+{
+    #[Route('/chat', name: 'chat')]
+    public function chat(): Response
+    {
+        return $this->render('chat.html.twig');
+    }
+}
