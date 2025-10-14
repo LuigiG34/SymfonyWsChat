66747b16a032e2f05138b1a42ef946457b4141da
Luigi Gandemer <luigigandemer6@gmail.com>
2025-10-14 14:02:29 +0200

    Security & Auth

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class AuthController extends AbstractController
+{
+    #[Route('/api/register', name: 'api_register', methods: ['POST'])]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $em,
+        ValidatorInterface $validator
+    ): JsonResponse {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['username']) || !isset($data['password'])) {
+            return $this->json(['error' => 'Username and password required'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $existingUser = $em->getRepository(User::class)->findOneBy(['username' => $data['username']]);
+        if ($existingUser) {
+            return $this->json(['error' => 'Username already taken'], Response::HTTP_CONFLICT);
+        }
+
+        $user = new User();
+        $user->setUsername($data['username']);
+        $user->setRoles(['ROLE_USER']);
+        
+        $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
+        $user->setPassword($hashedPassword);
+
+        $errors = $validator->validate($user);
+        if (count($errors) > 0) {
+            return $this->json(['error' => (string) $errors], Response::HTTP_BAD_REQUEST);
+        }
+
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json([
+            'message' => 'User registered successfully',
+            'username' => $user->getUsername()
+        ], Response::HTTP_CREATED);
+    }
+
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    public function login(): JsonResponse
+    {
+        return $this->json(['error' => 'This should not be reached'], Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    #[Route('/api/logout', name: 'api_logout', methods: ['POST'])]
+    public function logout(): JsonResponse
+    {
+        return $this->json(['message' => 'Logged out successfully']);
+    }
+
+    #[Route('/api/me', name: 'api_me', methods: ['GET'])]
+    public function me(): JsonResponse
+    {
+        $user = $this->getUser();
+        
+        if (!$user) {
+            return $this->json(['error' => 'Not authenticated'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->json([
+            'username' => $user->getUserIdentifier(),
+            'roles' => $user->getRoles()
+        ]);
+    }
+}
